9609bda276daf65c46da874ed966cb13b08dca37
renatopcinfo <renatopcinfo@gmail.com>
2020-10-11 01:27:54 -0300

    create

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -30,7 +30,22 @@ async function getGameInfo(slug) {
     short_description: description.textContent.trim().slice(0, 160),
     description: description.innerHTML,
   }
+}
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name })
+  return item.length ? item[0] : null
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName)
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true }),
+    })
+  }
 }
 
 module.exports = {
@@ -39,18 +54,12 @@ module.exports = {
 
     const { data: { products } } = await axios.get(gogApiUrl)
 
-    console.log(products[0])
+    //console.log(products[0])
 
-    await strapi.services.publisher.create({
-       name: products[0].publisher , 
-       slug: slugify(products[0].publisher).toLowerCase(),
-      })
+    await create(products[1].publisher, "publisher")
+    await create(products[1].developer, "developer")
 
-    await strapi.services.developer.create({
-        name: products[0].developer , 
-        slug: slugify(products[0].developer).toLowerCase(),
-       })
-
+    
     //console.log(await getGameInfo(products[1].slug))
   }
 };
